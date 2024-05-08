f0e82c7dbe65aed311c5dbb9fa48bd16fe26a4bf
Jatin Kumar <155115799+jatin800@users.noreply.github.com>
2024-05-09 02:31:57 +0530

    Counter

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+// function counter(n) {
+//   let counter = n;
+//   function increament() {
+//     counter++;
+//     return counter;
+//   }
+//   return increament;
+// }
+
+let myCounter = counter(5);
+console.log(myCounter());
+console.log(myCounter());
+console.log(myCounter());
+
+function counter(n) {
+  let counter = n;
+  function increament() {
+    const currentValue = counter;
+    counter++;
+    return currentValue;
+  }
+  return increament;
+}
